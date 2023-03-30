5f72217cb64f63846fd88ca0c37358a6441fd12c
tankhanhdao <tankhanhdao@gmail.com>
2023-03-30 09:31:03 -0700

    added base.html, login function and template

--- Question_Blueprint/Question_Blueprint/settings.py
+++ Question_Blueprint/Question_Blueprint/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     'crispy_forms',          
     'crispy_bootstrap5',
 
+    # tag model
+    'taggit',
 
 ]
 
@@ -64,7 +67,7 @@ ROOT_URLCONF = 'Question_Blueprint.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -125,7 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -137,6 +143,7 @@ AUTHENTICATION_BACKENDS = [
    'allauth.account.auth_backends.AuthenticationBackend'
 ]
 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
--- Question_Blueprint/user/views.py
+++ Question_Blueprint/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
+def login(request):
+    return redirect('login')
+
+def logout(request):
+    logout(request)
+    return redirect('logout')
+
