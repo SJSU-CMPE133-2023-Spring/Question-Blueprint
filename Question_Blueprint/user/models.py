8e5e2ebbe9c794a459ea2a91be7602a347ee63eb
tankhanhdao <tankhanhdao@gmail.com>
2023-04-04 22:05:30 -0700

    added user create and view profile

--- Question_Blueprint/user/models.py
+++ Question_Blueprint/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=1000)
+    image = models.ImageField(default='default.jpg', upload_to="profile_pic")
+    
+ 
+    def __str__(self):
+        return f'{self.user.username} - Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 800 or img.width > 800:
+            output_size = (800, 800)
+            img.thumbnail(output_size)
+            img.save(self.image.path) 
--- Question_Blueprint/user/views.py
+++ Question_Blueprint/user/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from main_app.models import Question
 # Create your views here.
-def login(request):
 
+
+def login(request):
     return redirect('login')
 
 
 def logout(request):
     logout(request)
-    
-    print('sdsdsdssdsdd')
     return redirect('logout')
 
 
@@ -27,3 +28,14 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form':form})
+
+
+@login_required
+def profile_view(request, username):
+    user1 = User.objects.get(username=username)
+    question_count = Question.objects.filter(user=user1).count()
+    context = {
+        'user1' : user1,
+        'question_count': question_count,
+    }
+    return render(request, 'user/profile.html', context=context)
