8faf8907b02d8b11ac9d187ac63355a8e552cc49
tankhanhdao <tankhanhdao@gmail.com>
2023-04-04 19:14:45 -0700

    modified profile and model

--- Question_Blueprint/user/models.py
+++ Question_Blueprint/user/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=1000)
     image = models.ImageField(default='default.jpg', upload_to="profile_pic")
+    
  
     def __str__(self):
         return f'{self.user.username} - Profile'
